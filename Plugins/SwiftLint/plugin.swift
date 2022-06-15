57f4ea73c6e180292d530980a26e97e6158cc2cf
Yonat Sharon <yonat@ootips.org>
2022-06-15 09:17:47 +0300

    Fix Xcode problems by using the workaround presented in WWDC22

--- Plugins/SwiftLint/plugin.swift
+++ Plugins/SwiftLint/plugin.swift
@@ -1,20 +1,29 @@
+import Foundation
 import PackagePlugin
 
 @main
 struct SwiftLint: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard let target = target as? SourceModuleTarget else { return [] }
+
+        // Create empty source file as output, as a workaround for rdar://92573384 (Source: https://developer.apple.com/videos/play/wwdc2022/102/)
+        let outputPath = context.pluginWorkDirectory.appending("Generated.swift")
+        try "".write(to: URL(fileURLWithPath: outputPath.string), atomically: false, encoding: .utf8)
+
         return [
             .buildCommand(
                 displayName: "Linting \(target.name)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
+                    "--quiet",
                     "--in-process-sourcekit", // alternative to the environment variable
                     "--path",
                     target.directory.string   // only lint the files in the target directory
                 ],
-                // environment: ["IN_PROCESS_SOURCEKIT": "YES"] // doesn't work in Xcode 13.3
-                environment: [:]
+                environment: [:],
+                inputFiles: target.sourceFiles(withSuffix: ".swift").map(\.path),
+                outputFiles: [outputPath]
             )
         ]
     }
